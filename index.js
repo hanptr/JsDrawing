5a2470fef5ab7494440e72e595ea045014471c7d
hanptr <hanczik.peter0214@gmail.com>
2024-05-12 16:12:58 +0200

    continous drawing implemented

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 let space = document.querySelector('#space')
 
-document.addEventListener('mousedown', draw)
+document.addEventListener('mousedown', startDrawing)
+document.addEventListener('mousemove', draw)
+document.addEventListener('mouseup', stopDrawing)
+
+let isDrawing = false;
+
+function startDrawing(e){
+    isDrawing = true;
+    draw(e);
+}
+
+function stopDrawing(){
+    isDrawing = false;
+}
 
 function draw(e){
+    //can only draw when the left mouse button is held down, when first holding it down the isDrawing is set to true so when I move the mouse
+    //since isDrawing is true I am able to continously draw. When i release the button the mouseup event fires setting the isDrawing to false
+    //thus returning 0. Might not be the most efficient way since when I move the mouse the draw() func gets called every time but since
+    //isDrawing = false, is return 0 every time.
+    if (!isDrawing) {
+        return 0;
+    }
     let star = document.createElement('div');
     star.classList.add('star');
     star.style.visibility = 'hidden';
@@ -22,7 +42,7 @@ function draw(e){
     spaceWidth = space.offsetWidth;
 
     let distanceFromTop = space.getBoundingClientRect().top;
-    let distanceFromLeft = space.getBoundingClientRect().left; // cuse the width is auto the distance from left and right is the same
+    let distanceFromLeft = space.getBoundingClientRect().left; // because the width is auto the distance from left and right is the same
 
 
 
